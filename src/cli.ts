74afd4b05db717e9d47ef70e408d0d33f5c6e8a5
lafkpages <luisafk@proton.me>
2024-06-17 17:51:41 +0200

    feat: cli Desmos URLs

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,44 @@ import { latexToDesmosLatex } from "./utils";
 const ce = new ComputeEngine();
 let iters = 100;
 
+const desmosLatexes: string[] = [];
+
+const server = Bun.serve({
+  fetch(request) {
+    const url = new URL(request.url);
+
+    const index = parseInt(url.pathname.slice(1));
+
+    if (isNaN(index)) {
+      return new Response("Invalid index", { status: 400 });
+    }
+
+    if (index < 0 || index >= desmosLatexes.length) {
+      return new Response("Index out of bounds", { status: 400 });
+    }
+
+    return new Response(null, {
+      status: 307,
+      headers: {
+        location: `https://www.desmos.com/calculator?desmitosFetch=1&desmitosState=data:application/json;utf-8,${encodeURIComponent(
+          JSON.stringify({
+            version: 11,
+            expressions: {
+              list: [
+                {
+                  type: "expression",
+                  id: "1",
+                  latex: latexToDesmosLatex(desmosLatexes[index]),
+                },
+              ],
+            },
+          })
+        )}`,
+      },
+    });
+  },
+});
+
 console.write("> ");
 for await (const line of console) {
   const n = parseFloat(line);
@@ -29,16 +67,20 @@ for await (const line of console) {
     if (!Bun.deepEquals(mathJson, [])) {
       const box = ce.box(mathJson, { canonical: false });
 
-      console.log("Desmos LaTeX (boxed):", latexToDesmosLatex(box.latex));
-      //   console.log(
-      //     "Desmos LaTeX (unboxed):",
-      //     latexToDesmosLatex(ce.serialize(mathJson))
-      //   );
+      const desmosLatex = latexToDesmosLatex(box.latex);
+      console.log("Desmos LaTeX (boxed):", desmosLatex);
+      desmosLatexes.push(desmosLatex);
+
       console.log("Math JSON:", mathJson);
 
       try {
         console.log("Compiled:", box.compile()?.({}));
       } catch {}
+
+      console.log(
+        `Desmos URL:`,
+        new URL("/" + (desmosLatexes.length - 1), server.url).href
+      );
     }
   }
 
