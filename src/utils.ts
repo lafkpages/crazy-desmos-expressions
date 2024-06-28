ba90cb352b76b64944d08c5613f99238d9a374e7
lafkpages <luisafk@proton.me>
2024-06-28 11:56:51 +0200

    feat: latexToDesmosLatex \lbrace and \rbrace

--- src/utils.ts
+++ src/utils.ts
@@ -64,6 +64,8 @@ export function latexToDesmosLatex(latex: string | String) {
     .replace(/\)/g, "\\right)")
     .replace(/\\bot/g, "1\\lt0")
     .replace(/\\top/g, "1\\gt0")
+    .replace(/\\lbrace/g, "\\left\\{")
+    .replace(/\\rbrace/g, "\\right\\}")
     .replace(/\\exponentialE/g, " e")
     .replace(/\\l(floor|ceil)/g, "\\operatorname{$1}\\left(")
     .replace(/\\r(floor|ceil)/g, "\\right)")
